eca9b3126713a6503588273b0135d2aa7f91187a
bigmb <malav.b93@gmail.com>
2022-11-20 05:26:06 +0000

    __init__.py add

--- setup.py
+++ setup.py
@@ -4,16 +4,15 @@ from setuptools import setup,find_packages,find_namespace_packages
 from mb_utils.src.version import version
 
 setup(
-    name="bigmb.utils",
+    name="bigmb_utils",
     version=version,
     description="Basic logger and utils package",
     author=["Malav Bateriwala"],
-    packages=find_namespace_packages(include=["bigmb.*"]),
+    packages=find_namespace_packages(),
     #packages=find_packages(),
     scripts=[],
     install_requires=[
         "numpy",
         "pandas",
-        "logging",
         "colorama",],
     python_requires='>=3.8',)
