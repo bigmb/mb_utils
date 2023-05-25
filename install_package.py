bd3c3a164800cbcda2ad4d0d7e28e506664f7000
bigmb <malav.b93@gmail.com>
2023-05-25 01:46:29 +0100

    bug fix

--- install_package.py
+++ install_package.py
@@ -10,13 +10,25 @@ file = '/home/malav/basic_mb'
 #subprocess.run(["cd",file]), check=True, stdout=subprocess.PIPE).stdout
 os.system('cd ' + file)
 
+os.system('./make_version.sh')
+
+print("version file updated")
+
 if os.path.exists(file+'/dist'):
-    os.system('rm -rf '+file+'/dist')
-    os.system('rm -rf '+file+'/build')
+    os.system('sudo rm -rf '+file+'/dist')
+    os.system('sudo rm -rf '+file+'/build')
 #subprocess.run(["ls"]),check=True, stdout=subprocess.PIPE).stdout
 os.system("ls")
 subprocess.run(["git", "pull"], check=True, stdout=subprocess.PIPE).stdout
 #os.system('git pull')
+print('git pull done')
+print('*'*100)
 os.system('python3.8 -m setup bdist_wheel')
+
+print('*'*100)
+print('wheel built')
 os.system('python3.8 -m pip install '+file + '/dist/' +os.listdir(file +'/dist')[-1])
+
+print('package installed')
+print('*'*100)
 os.system('python3.8 -m twine upload dist/*')
