eab0829fc61558cb953e7d1d155b6eeafb012f75
Malav Bateriwala <malav.bateriwala@winnowsolutions.com>
2023-06-06 16:07:20 +0100

    Bug fix commit

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     version=version,
     description="Basic logger and utils package",
     author=["Malav Bateriwala"],
-    packages=find_namespace_packages(include=["mb_utils.*"]),
-    #packages=find_packages(),
+    #packages=find_namespace_packages(include=["mb_utils.*"]),
+    packages=find_packages(),
     scripts=[],
     install_requires=[
         "numpy",
